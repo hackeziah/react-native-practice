c455ce8eafbd3109b0928a14b9e0fb0b4f6a7d16
hackeziah <lamadridkevinpaul@gmail.com>
2021-08-24 01:46:26 +0800

    adding Modal and pickeritems

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TextInput,
+  Switch,
+} from "react-native";
 import AppButton from "./app/components/AppButton";
 import Screen from "./app/components/Screen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
@@ -12,9 +20,44 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [data, setData] = useState(false);
+  const categories = [
+    { label: "Pizza", value: 1 },
+    { label: "Burger", value: 2 },
+    { label: "Drinks", value: 3 },
+  ];
+
+  const [category, setCategory] = useState(categories[0]);
   return (
-    <ListingsScreen  />
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Catgory"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+    // <Switch value={data} onValueChange={(change) => setData(change)} />
+    //
+    // <AppTextInput placeholder="Name" icon="email" />
+    // <Screen>
+    //   <Text>{firstName}</Text>
+    //   <TextInput
+    //     secureTextEntry={true}
+    //     keyboardType="numeric"
+    //     clearButtonMode="always"
+    //     onChangeText={(text) => setFirstName(text)}
+    //     placeholder="First Name"
+    //     style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+    //   />
+    // </Screen>
+    // <ListingsScreen  />
     // <AccountScreen />
 
     // <Screen>
