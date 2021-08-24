f596d9b1537909cb0489d9f60305f3fa6cfb6369
hackeziah <lamadridkevinpaul@gmail.com>
2021-08-25 01:17:09 +0800

    adding background for login

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, ImageBackground } from "react-native";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
@@ -9,28 +9,33 @@ function LoginScreen(props) {
 
   return (
     <Screen>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <ImageBackground
+        style={styles.background}
+        source={require("../assets/background.jpg")}
+      >
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
-      <AppTextInput
-        icon="email"
-        autoCorrect={false}
-        autoCapitalize="none"
-        placeholer="Email"
-        keyboardType="email-address"
-        textContentType="emailAddress"
-        onChangeText={(text) => setUsername(text)}
-      />
+        <AppTextInput
+          icon="email"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholer="Email"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          onChangeText={(text) => setUsername(text)}
+        />
 
-      <AppTextInput
-        icon="lock"
-        autoCorrect={false}
-        autoCapitalize="none"
-        placeholer="Password"
-        textContentType="password"
-        secureTextEntry
-        onChangeText={(text) => ser(text)}
-      />
-      <AppButton title="Login" onPress={() => console.log("Test")} />
+        <AppTextInput
+          icon="lock"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholer="Password"
+          textContentType="password"
+          secureTextEntry
+          onChangeText={(text) => ser(text)}
+        />
+        <AppButton title="Login" onPress={() => console.log("Test")} />
+      </ImageBackground>
     </Screen>
   );
 }
@@ -45,6 +50,9 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 50,
     marginBottom: 20,
+  },
+  background: {
+    flex: 1,
   },
 });
 
